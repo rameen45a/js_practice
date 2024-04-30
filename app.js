c1fe636360ada0a1f15ecfa57b73dc2f2d36fa02
Ramin <168584750+rameen45a@users.noreply.github.com>
2024-04-30 23:48:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,194 @@
+// let a = "ramin"
+// console.log(a)
+// a = 2
+// b = 3
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(Math.random());
+// console.log(Math.floor (Math.random()*100));
+// practice chap no 3
+//arry number start from index=0
+// let arr1 = [34,53,56,67,78,90,34]
+// console.log( "index of last array",arr1.length-1)
+// let arr2 =["anc","abc","efg","bnm"]
+// console.log("array.length",arr2)
+// arr4 = [10]
+// console.log(arr4);
+// // array methods
+// let fruits = ['apple','banana','cherry'];
+// fruits.sort();
+// console.log('sorted',fruits);// sorted apple banana cherry
+// fruits.reverse() // reverse the order
+// fruits[fruits.length]= 'orange'; // add orange to the end of fruits[]
+// console.log('add',fruits);//['apple','banana','cherry','orange']
+// fruits.push ('grape'); // add grape at the end of fruits[]
+// console.log('push',fruits)//['apple','banana','cherry','orange','grape']
+// fruits.unshift ("kiwi");   /// add kiwi in first position and shift all other elements one
+// console.log('unshift',fruits)//['kiwi','apple','banana','cherry','orange','grape']
+// fruits.pop();    //// remove element from the end of an array
+// console.log('pop',fruits)//['kiwi','apple','banana','cherry','orange' ]
+// fruits.shift() ;////remove element form beginning of an array
+// console.log('shift',fruits)//['apple','banana','cherry','orange' ]
+// delete fruits[0];/// delete item by its index value
+// console.log('delete',fruits)//['banana','cherry','orange' ]
+// fruits.splice(-2 ,1 );/// remove two items starting with second last item
+// console.log('splice',fruits)//['banana' ]
+
+// for (i=0; i<fruits.length; i++){
+//     console.log(`looping through ${i}=>${fruits[i]}`)
+//     }
+//     if (fruits.includes("banana")){
+//         console.log("we have banana")
+//         }else{
+//             console.log("no banana")};
+//             // chap 1-9,11,15to 17 for quiz
+//             let x = "ramin"
+//             console.log(x)
+//             alert("Hello World!")
+//             prompt("what is Your Name?")
+
+// let arr1 = [22,11,44,55,34]
+// console.log( "index of last array",arr1.length-1)
+// //objects practice//
+// let person={name:"Ramin",age:30,city:"Tehran"}
+// console.log(person)
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.city)
+// //accessing object properties using dot notation
+// console.log(person.address="tehran")
+// //accessing object properties using bracket notation
+// console.log(person["address"]="tehran")
+// //adding new property to existing objects
+// person.job="developer"
+// console.log(person)
+// //deleting a property from an object
+// delete person.job
+// console.log(person)
+// let person =
+//     name: "Ramin",
+//         age: 30,
+//             city: "Tehran",
+//                 Works: false,
+//                     courses: {
+//     compulsory: ["english", "urdu", "phsics"],
+//         optional: "maths",
+//             total: 2,
+//                 completed: false
+// }
+// // console.log(typeof person)
+// // console.log(person.age)
+// // console.log(person["name"])
+// // console.log(person.courses.total)
+// console.log(person.courses.compulsory)
+// let i = 0;
+// while (i < 6) {
+//     console.log(i);
+//     i++
+// }
+//2 table print//
+// let j = 1;
+// while (j <= 10) {
+//     console.log(j * 2);
+//     j++
+// }
+// //3table print//
+// let k = 1;
+// while (k <= 10) {
+//     console.log(k * 3);
+//     k++
+// }
+// let someArray = ["Mike", "Antal", "Marc", "Emir", "Louiza", "Jacky"];
+// let notFound = true;
+// while (notFound && someArray.length > 0) {
+//     if (someArray[0] === "Louiza") {
+//         console.log("Found her!");
+//         notFound = false;
+//     } else {
+//         someArray.shift();
+//     }
+// }
+// let number;
+// do {
+//     number = prompt("Please enter a number between 0 and 100: ");
+// } while (!(number >= 0 && number < 100));
+// console.log(number);
+// for (let i =0; i<10; i++) {
+//     // code to be executed
+//     console.log(i);
+//     }
+//     let arrOfArrays = [];
+//     for (let i = 0; i < 3; i++){
+//     arrOfArrays.push([]);
+//     for (let j = 0; j < 7; j++) {
+//     arrOfArrays[i].push(j);
+//     }
+//     }
+//     document.write(arrOfArrays);
+//     //chap 24 practices............//
+
+    // let date = new Date() ;
+    // console.log(date);
+
+    // var newDay = new Date("Nov 30 , 2023");
+    // let dif = newDay-date
+    // console.log(dif);
+    // let difDays = dif/1000/60/60/2
+    // 4;
+    // console.log(difDays);
+    // let difHours = dif/1000/60/60
+    // console.log(difHours);
+    // let difMinutes = dif/1000/60
+    // console.log(difMinutes);
+    // let difSeconds = dif/1000
+    // console.log(difSeconds);
+    // let date = new Date()
+    // console.log(date);
+    // let rain = false;
+    // if (rain) {
+    //     console.log("its raining Today");
+    // }
+    // else{
+    //     console.log(" it will not raining Today");
+    // }
+    // let x = 10;
+    // switch (x){
+    //     case 10:
+    //         console.log("x is 10");
+    //         break;
+    //         default :
+    //         console.log("x is not 10");
+    //         }
+    //         let y = 10;
+    //         switch (y){
+    //             case 10:
+    //                 console.log("y is 10");
+    //                 break;
+    //                 default :
+    //         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
---
+++ js.js
@@ -0,0 +1,14 @@
+// function myfunction(){
+//   document.getElementById('demo').innerHTML="paragraph changed";
+// }
+// document.getElementById('demo').innerHTML = 3 + 4;
+// document.write(3 + 4);
+// window.alert(5 + 6)
+// alert("3" + 4 - 5  );
+// console.log(5+7)
+const car = ['toyota','BMW','prado','hirof']
+console.log(car[0]);
+car.push('audi');
+console.log('push',car);
+car.unshift('merenda');
+console.log('unshift',car);
